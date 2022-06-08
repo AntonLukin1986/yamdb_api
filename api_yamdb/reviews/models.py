d4d3e4fe12ed873b3cfb20b1a40a517524b7b02e
Антон Лукин <shahter86@mail.ru>
2022-06-08 17:09:59 +0300

    .

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import constraints
 from django.utils import timezone
-
 from users.models import User
 
 
@@ -112,7 +110,7 @@ class Review(models.Model):
         verbose_name_plural = 'Отзывы'
         ordering = ['-pub_date']
         constraints = (
-            constraints.UniqueConstraint(
+            models.UniqueConstraint(
                 fields=['author', 'title'], name='unique'
             ),
         )
